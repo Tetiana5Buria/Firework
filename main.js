cfde1a3c06a9d3b6b7fc756bfee458c3e6f1bf1b
Tetiana5Buria <poriniya@gmail.com>
2025-11-27 16:13:43 +0200

    commit

---
+++ main.js
@@ -0,0 +1,18 @@
+import { Fireworks } from "fireworks-js";
+
+const container = document.querySelector(".container");
+const fireworks = new Fireworks(container, {
+  brightness: { min: 50, max: 80 },
+  traceLength: 3,
+  traceSpeed: 5,
+  explosionRadius: 50,
+  autoresize: true,
+});
+fireworks.start();
+const boom = new Audio("/dist/boom1.mp3");
+boom.play();
+/* window.addEventListener("click", () => {
+  boom.currentTime = 0;
+}); */
+
+//fireworks.stop();

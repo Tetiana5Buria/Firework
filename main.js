231551568ffa6388131934ac13f2bddc43771111
Tetiana5Buria <poriniya@gmail.com>
2025-11-28 11:53:56 +0200

    sounds+animation

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-import { Fireworks } from "fireworks-js";
-
-const container = document.querySelector(".container");
-const fireworks = new Fireworks(container, {
-  brightness: { min: 50, max: 80 },
-  traceLength: 3,
-  traceSpeed: 5,
-  explosionRadius: 50,
-  autoresize: true,
+window.addEventListener("DOMContentLoaded", () => {
+  const container = document.querySelector(".container");
+  const fireworks = new Fireworks.default(container, {
+    brightness: { min: 50, max: 80 },
+    traceLength: 3,
+    traceSpeed: 5,
+    explosionRadius: 50,
+    autoresize: true,
+    sound: true,
+  });
+  fireworks.start();
+  const boom = new Audio("boom1.mp3");
+  document.addEventListener(
+    "click",
+    () => {
+      boom.currentTime = 0;
+      boom.play();
+    },
+    { once: true }
+  );
+  //fireworks.stop();
 });
-fireworks.start();
-const boom = new Audio("./dist/boom1.mp3");
-boom.play();
-window.addEventListener("click", () => {
-  boom.currentTime = 0;
-});
-
-//fireworks.stop();
